f45b8159d06e95fb6569cbc4f87f88f17e6c26c6
mobisa-012 <mobisakwamboka@gmail.com>
2023-09-16 15:51:49 +0300

    homepage

---
+++ tasky/src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function home() {
+    return (
+        <div>
+            <h1 className="text-center">Welcome to the Home Page</h1>
+            <p>Welcome to the Home page</p>
+        </div>
+    );
+}
+
+export default home;
